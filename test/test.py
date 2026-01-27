01d0118b9ee8a583b16ddfc591f9d55b3eb35821
Fing2525 <phuritatfing@gmail.com>
2026-01-27 12:58:34 +0700

    python syntax fix

--- test/test.py
+++ test/test.py
@@ -23,13 +23,12 @@ async def test_project(dut):
     # await ClockCycles(dut.clk, 10)
     # dut.rst_n.value = 1
     dut.rst_n.value = 0
-for _ in range(20):
-    await RisingEdge(dut.clk)
+    for _ in range(20):
+        await RisingEdge(dut.clk)
 
-dut.rst_n.value = 1
-for _ in range(50):
-    await RisingEdge(dut.clk)
-
+    dut.rst_n.value = 1
+    for _ in range(50):
+        await RisingEdge(dut.clk)
 
     dut._log.info("Test project behavior")
 
