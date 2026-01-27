552f1a3ad427080f0bcd7d6dcb14b9dffed1d9db
Fing2525 <phuritatfing@gmail.com>
2026-01-27 14:59:50 +0700

    reset active high

--- test/test.py
+++ test/test.py
@@ -34,14 +34,14 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.rst_n.value = 1
 
     # ------------------------------------------------------------
     # Reset
     # ------------------------------------------------------------
     dut._log.info("Applying reset")
     await ClockCycles(dut.clk, 20)
-    dut.rst_n.value = 1
+    dut.rst_n.value = 0
     await ClockCycles(dut.clk, 50)
 
     # ------------------------------------------------------------
