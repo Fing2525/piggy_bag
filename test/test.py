aaf550018fa0c67b4019a70133bc6ebc9f90bc24
Fing2525 <phuritatfing@gmail.com>
2026-01-27 14:46:50 +0700

    fix reset to active high

--- test/test.py
+++ test/test.py
@@ -31,16 +31,15 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
+    dut.rst_n.value = 1
 
     # ------------------------------------------------------------
     # Reset
     # ------------------------------------------------------------
     dut._log.info("Applying reset")
     await ClockCycles(dut.clk, 20)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 50)
     dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 50)
 
     # ------------------------------------------------------------
     # Ensure UART idle (TX must be HIGH)
