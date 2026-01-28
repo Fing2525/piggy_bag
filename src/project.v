9ebb1bfb67e091dfcf1fd258249a5c4e2d9339bc
Fing2525 <phuritatfing@gmail.com>
2026-01-28 12:58:48 +0700

    test counter8bit

--- src/project.v
+++ src/project.v
@@ -19,6 +19,8 @@ module tt_um_piggy_top(
     wire _unused_uio = |uio_in;
 
     wire deb_out;
+    wire amount0;
+    wire change0;
 
     debouncer u_deb (
         .clk   (clk),
@@ -26,6 +28,8 @@ module tt_um_piggy_top(
         .Input (ui_in[2]),
         .Output(deb_out)
     );
+
+    Counter8bit u_cnt0 (.clk(clk), .reset(rst_n), .coin(deb_out), .amount(amount0), .change(change0));
 
     assign uo_out[0] = deb_out;
     assign uo_out[7:1] = 7'b0;
